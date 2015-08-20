402fa937eda225991a6f39ad91206b92f857274d
antonk <anton.kann@gmail.com>
2015-08-20 14:07:02 +0100

    initial commit

---
+++ public/scripts/main.js
@@ -0,0 +1,52 @@
+// jshint devel:true
+(function() {
+
+  $(document).ready(function() {
+    play();
+  })
+
+  var shuffle_it = function (e) {
+    $('.cup').removeClass('winner');
+    var shuffle = _.sample( [ "75px", "275px", "475px" ] );
+    $('.shell').css("left", shuffle); // move the shell
+    return shuffle;
+  }
+
+  var play = function() {
+    var shuffle = shuffle_it();
+
+    if (shuffle === '75px') {
+      $('.cup.one').addClass('winner');
+
+    } else if (shuffle === '275px') {
+        $('.cup.two').addClass('winner');
+
+    } else {
+        $('.cup.three').addClass('winner');
+    }
+
+    $('button.shuffle').text('Go!');
+  }
+
+  var check_win = function(e) {
+    if ( $(e.currentTarget).hasClass('winner') ) {
+      alert("You Win!");
+    } else {
+      alert("You Lose!");
+    }
+  }
+
+  $('button.shuffle').click(function() {
+    $(this).text('Shuffling...');
+    setTimeout(function(){
+       play();
+    }, 2000);
+  });
+
+  $('.cup').click(function(e) {
+    check_win(e);
+  })
+
+
+
+}());
